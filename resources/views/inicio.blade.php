b6104434483bd418525c8917d9077af1265dd730
Antonio Juan <antoniojrguez27@gmail.com>
2024-11-18 19:43:52 +0100

    tailwind instalado

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl bg-blue-300 font-bold underline">
+    Hello world!
+  </h1>
+    <div class="bg-black">Tailwind worksaaaaaaaaa</div>
+    <div class="bg-red-500">Tailwind worksaaaaaaaaa</div>
+    <section>
+      <div class="w-40 h-40 bg-blue-400 mt-32 ml-32 rounded-full">hello world 2</div>
+    </section>
+</body>
+</html>
